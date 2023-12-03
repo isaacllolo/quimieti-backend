29462f3eb88e638b731d3101dc97aab2bee4e7da
Isaac David Llorente Lopez <82958519+isaacllolo@users.noreply.github.com>
2023-12-03 18:24:05 -0500

    env variables db.js

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
 
