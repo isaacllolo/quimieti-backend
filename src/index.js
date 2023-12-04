5ebded9120b40835adbe2ee602767fdad1e6acfd
Isaac David Llorente Lopez <82958519+isaacllolo@users.noreply.github.com>
2023-12-03 22:05:21 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,27 +38,13 @@ const knex = Knex({
 const PgSession = pgSession(session); // Crea una instancia de pgSession
 
 const app = express();
-
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://quimieti-frontend.onrender.com"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-
-  next();
-});
+app.set('trust proxy')
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD', 'PATCH', 'CONNECT', 'TRACE'],
+    credentials: true,
+}));
+app.use(express.static('public'));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
