fdb446205de7780315b803e7fd489ceb29b055a5
Isaac David Llorente Lopez <82958519+isaacllolo@users.noreply.github.com>
2023-12-03 20:07:01 -0500

    token test

--- src/index.js
+++ src/index.js
@@ -58,9 +58,7 @@ app.use(cookieParser());  // Usa cookie-parser para gestionar cookies
 app.use(cors({
   allowedOrigins: process.env.CORS_ORIGIN,
   origin: process.env.CORS_ORIGIN,
-  headers: 'Content-Type, Authorization ,set-cookie',
-
-  accessControlAllowOrigin: process.env.CORS_ORIGIN,
+  headers: 'Content-Type, Authorization',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
