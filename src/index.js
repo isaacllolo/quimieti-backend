ab280a24829ac7ad3df4943f1fc36c54e82972d7
Isaac David Llorente Lopez <82958519+isaacllolo@users.noreply.github.com>
2023-12-05 10:32:11 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -57,8 +57,9 @@ app.use(
       tableName: 'session',
     }),
     cookie: {
-      secure: process.env.NODE_ENV === 'production',
+      maxAge: 1000 * 60 * 60 * 24 * 7,
       sameSite: 'none',
+      secure: true,
     },
   })
 );
