f07870cc40ff116beb92905e36aa009f9b03912b
Isaac David Llorente Lopez <82958519+isaacllolo@users.noreply.github.com>
2023-12-07 19:53:43 -0500

    Update verifyTokenController.js

--- src/controllers/verifyTokenController.js
+++ src/controllers/verifyTokenController.js
@@ -1,29 +1,29 @@
 // controllers/verifyTokenController.js
 import jwt from 'jsonwebtoken';
 import { pool } from '../db.js';
-import cookieParser from 'cookie-parser';
+
 const verifyToken = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
     console.log( 'HEADERS: ',req.headers);
     console.log( 'COOKIES: ',req.cookies);
     console.log( 'BODY: ',req.body);
-    const token = req.body.split('token=')[1];
+    const token = req.headers.cookie.split('token=')[1];
 
-    // if (!token) {
-    //     return res.status(401).json({ mensaje: 'Token no proporcionado' });
-    // }
+    if (!token) {
+        return res.status(401).json({ mensaje: 'Token no proporcionado' });
+    }
 
     try {
-    //     const decoded = jwt.verify(token, process.env.SESSION_SECRET);
-    //     console.log('Decoded:', decoded);
+        const decoded = jwt.verify(token, process.env.SESSION_SECRET);
+        console.log('Decoded:', decoded);
 
-    //     const userResult = await pool.query('SELECT * FROM usuarios WHERE id = $1', [decoded.userId]);
-    //    // console.log('User Result:', userResult.rows);
+        const userResult = await pool.query('SELECT * FROM usuarios WHERE id = $1', [decoded.userId]);
+       // console.log('User Result:', userResult.rows);
 
-    //     if (userResult.rows.length === 0) {
-    //         return res.status(401).json({ mensaje: 'Usuario no encontrado' });
-    //     }
-    //     res.setHeader('Access-Control-Allow-Methods', 'POST');
+        if (userResult.rows.length === 0) {
+            return res.status(401).json({ mensaje: 'Usuario no encontrado' });
+        }
+        res.setHeader('Access-Control-Allow-Methods', 'POST');
         res.status(200).json({ mensaje: 'Token válido',status: "success" , usuario: userResult.rows[0].usuario } );
     } catch (error) {
         console.error('Error al verificar el token:', error);
