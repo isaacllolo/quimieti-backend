04b8cb8dbc89afabe25b8eb2b2e84cce150673b1
Isaac David Llorente Lopez <82958519+isaacllolo@users.noreply.github.com>
2023-12-09 13:05:18 -0500

    Update leccionesController.js

--- src/controllers/leccionesController.js
+++ src/controllers/leccionesController.js
@@ -8,9 +8,9 @@ const obtenerInformacionSlidesPorLeccion = async (req, res) => {
 
   try {
     console.log('Recibiendo solicitud de información de slides por lección:', req.params.id);
-    const { id } = req.params.id;
+    const  Id  = req.params.id;
     console.log('ID de la lección:', req.params.id);
-    const result = await pool.query('SELECT * FROM slides WHERE leccion_id = $1', [id]);
+    const result = await pool.query('SELECT * FROM slides WHERE leccion_id = $1', [Id]);
     res.setHeader('Access-Control-Allow-Methods', 'POST');
     if (result.rows && result.rows.length > 0) {
       res.status(200).json(result.rows);
