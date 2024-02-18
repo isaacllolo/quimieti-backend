7d9dfbdb8fa40101287fd7461da2dd08cdc4b576
Isaac David Llorente Lopez <isaaacllolo@gmail.com>
2024-02-18 11:32:53 -0500

    Update Register.js

--- src/controllers/Register.js
+++ src/controllers/Register.js
@@ -44,7 +44,6 @@ const Register = async (req, res) => {
     const newUser = result.rows[0];
     delete newUser.contrasena;
 
-    res.setHeader('Access-Control-Allow-Methods', 'POST');
 
     res.status(201).json({ mensaje: 'Usuario registrado exitosamente' });
   } catch (error) {
